91ea171bd3075e2889f401035c457c57b60be3d2
Suhrit Padakanti <suhrit@ucsb.edu>
2025-04-06 21:17:28 -0700

    xy - updated with actual developer name

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -20,7 +20,7 @@ public class Developer {
         // You may use just the name that is used on <https://bit.ly/cs156-s25-teams>
         // i.e. your first name, or your first and initial of last name
 
-        return "Chris G.";
+        return "Suhrit Padakanti";
     }
 
     /**
@@ -30,7 +30,7 @@ public class Developer {
 
     public static String getGithubId() {
         // TODO: Change this to your github id
-        return "cgaucho";
+        return "suhritpadakanti";
     }
 
     /**
@@ -40,13 +40,12 @@ public class Developer {
     
     public static Team getTeam() {
         // TODO: Change this to your team name
-        Team team = new Team("s25-xx");
-        team.addMember("Alice");
-        team.addMember("Bob");
-        team.addMember("Chris G.");
-        team.addMember("Danny");
-        team.addMember("Eve");
-        team.addMember("Frances");
+        Team team = new Team("s25-16");
+        team.addMember("Christian");
+        team.addMember("Derek");
+        team.addMember("Jeffrey");
+        team.addMember("John");
+        team.addMember("Wilson");
         return team;
     }
 }
